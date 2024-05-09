1dd5fb3f8ff55712c50b6b615784b1d82b805bb9
gopimb <coolgopichand@gmail.com>
2024-05-09 19:05:26 +0530

    Update UserController.java

--- UserRegistration1/src/main/java/io/aadeesh/controller/UserController.java
+++ UserRegistration1/src/main/java/io/aadeesh/controller/UserController.java
@@ -1,10 +1,10 @@
 package io.aadeesh.controller;
 
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
