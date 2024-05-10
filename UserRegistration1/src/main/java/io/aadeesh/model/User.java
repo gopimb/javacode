8b17a317d4ebbb3213adb62ca50f855658224b49
gopimb <coolgopichand@gmail.com>
2024-05-10 18:49:41 +0530

    Update User.java

--- UserRegistration1/src/main/java/io/aadeesh/model/User.java
+++ UserRegistration1/src/main/java/io/aadeesh/model/User.java
@@ -1,9 +1,9 @@
 package io.aadeesh.model;
-
+import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 
 @Entity
 public class User 
